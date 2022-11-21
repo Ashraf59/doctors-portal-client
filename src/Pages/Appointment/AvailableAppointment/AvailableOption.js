b48ba8860e6d9a003205cadb471c3f806e52f3ed
Ashraf59 <ashrafams59b@gmail.com>
2022-11-22 00:24:19 +0600

    Price added

--- src/Pages/Appointment/AvailableAppointment/AvailableOption.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableOption.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const AvailableOption = ({appointmentoption, setTreatment}) => {
-    const {name, slots} = appointmentoption;
+    const {name, price, slots} = appointmentoption;
     return (
         <div className="card shadow-xl">
         <div className="card-body text-center">
           <h2 className="text-2xl font-bold text-secondary text-center">{name}</h2>
           <p>{slots.length > 0? slots[0] : 'No schedule today'}</p>
           <p>{slots.length} {slots.length > 1? 'spaces' : 'space'} Available</p>
+          <p><small>Price: ${price}</small></p>
           <div className="card-actions justify-center">
             <label 
             disabled = {slots.length === 0}
